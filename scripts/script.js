71222656985c2a301b3f2430435313a35167c6db
Stefan Espant <info@espant.com>
2022-09-14 15:27:18 +0200

    v2.0
    Nieuwe Illustraties. Nieuwe layout met een ronddraaiend sterrenstelsel en een dynamische achtergrond gebouwd door Luuk

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-test
+$('.ovals .oval').each(function() {
+    var delay = 0,
+    step = 60 / $(this).find('.carousel__item').length;
+    $(this).find('.carousel__item').each(function() {
+      $(this).css('animation-delay', -delay + "s");
+      delay += step;
+    })
+  })
+
+  
